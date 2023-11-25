d57964514e8423e1d867c2770ade202939c4d4da
unknown <hoangdean01@gmail.com>
2023-11-26 01:20:23 +0700

    update

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,11 @@
 console.log("here");
+
+/**
+$('#random').on('mouseover',function() {
+    var color = '#'+Math.floor(Math.random()*16777215).toString(16);
+    var colorString = '0px 0px 30px 0px ' + color;
+    $('#random').css('box-shadow',colorString);
+    $('#random').css('-webkit-box-shadow',colorString);
+    $('#random').css('-mox-box-shadow',colorString);
+});
+*/
